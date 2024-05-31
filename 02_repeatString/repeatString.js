8477f3268d83d402c36fad789ceb74eb874d6afd
randyaf <randyaf29@gmail.com>
2024-06-01 06:52:47 +0700

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, repeatNumber) {
+    if (repeatNumber < 0) return "ERROR";
+    let repeatedWord = "";
+    for (let i = 0; i < repeatNumber; i++) {
+        repeatedWord += word;
+    }
 
+    return repeatedWord;
 };
 
 // Do not edit below this line
