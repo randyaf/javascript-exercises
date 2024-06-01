3aa34dfa5ded99e63c24e0c86bcdd71dc4f29cd6
randyaf <randyaf29@gmail.com>
2024-06-01 08:36:08 +0700

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove) {
+    let modifiedArray = [...array];
+    for (item of modifiedArray) {
+        for (argument of arguments) {
+            while (modifiedArray.includes(argument)) {
+                modifiedArray.splice(modifiedArray.indexOf(argument), 1);
+            }
+        }
+    }
+    // for (item of modifiedArray) {
+    //   for (argument of arguments) {
+    //     if (item === argument) {
+    //       while(modifiedArray.includes(argument)) {
+    //         modifiedArray.splice(modifiedArray.indexOf(item), 1);
+    //       }
+    //     }
+    //   }
+    // }
+    return modifiedArray;
+
+    
 };
 
 // Do not edit below this line
