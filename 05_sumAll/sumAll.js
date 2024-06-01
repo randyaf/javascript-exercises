acee6b4b6461475550d78e540728070a5e7a7f89
randyaf <randyaf29@gmail.com>
2024-06-01 10:05:28 +0700

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if ((from < 0 || to < 0)
+    || (typeof from !== "number" || typeof to !== "number")) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (from < to) {
+        for (let i = from; i <= to; i++) sum += i;
+    } else {
+        for (let i = to; i <= from; i++) sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
