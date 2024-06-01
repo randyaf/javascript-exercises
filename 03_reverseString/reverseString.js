1a52fb41d073386a15039b1b6401c9740f941047
randyaf <randyaf29@gmail.com>
2024-06-01 07:00:29 +0700

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedString = "";
+    for (character of word) {
+        reversedString = character + reversedString;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
