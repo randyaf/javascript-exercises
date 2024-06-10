236854312afac87f9134a1bc029ef3f3c3422977
randyaf <randyaf29@gmail.com>
2024-06-10 09:10:37 +0700

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const regex = /[a-z0-9]+/g;
+    const result = words.toLowerCase().match(regex);
+    console.log(result);
+    const newWords = result.join("");
 
+    let tempWord = [];
+    for (let i = newWords.length -1; i >= 0; i--) {
+        tempWord.push(newWords.charAt(i));
+    }
+    const reversedWord = tempWord.join("");
+
+    return newWords === reversedWord ? true : false;
 };
 
 // Do not edit below this line
