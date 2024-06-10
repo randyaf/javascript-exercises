132d79164d7834e4b2adb9f72f3b24e218c2e7da
randyaf <randyaf29@gmail.com>
2024-06-10 10:39:00 +0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.map(person => {
+        let age = 0;
+        if (person.yearOfDeath === undefined) {
+            age = (new Date().getFullYear()) - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return {
+            name: person.name,
+            age,
+        }
+    }).reduce((oldest, person) => {
+        return oldest.age > person.age ? oldest: person;
+    });
 };
+
+const people = [
+    {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+    },
+    {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+    },
+    {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+    },
+]
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
