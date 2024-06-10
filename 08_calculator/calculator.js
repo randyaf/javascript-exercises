ff25069081a448ab05eafaaca59c2d1bab7291fa
randyaf <randyaf29@gmail.com>
2024-06-10 08:31:05 +0700

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) return 0;
+
+  return array.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  if (array.length === 0) return 0;
 
+  return array.reduce((acc, item) => acc * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
+
+console.log(factorial(2));
 
 // Do not edit below this line
 module.exports = {
